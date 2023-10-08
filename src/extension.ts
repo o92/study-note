a26d8ab547218e61b5cc85dbd5ba7ab41457ce85
o92 <huyuanjing1998@icloud.com>
2023-10-08 12:06:58 +0800

    init

---
+++ src/extension.ts
@@ -0,0 +1,12 @@
+import * as vscode from "vscode";
+
+export function activete(context: vscode.ExtensionContext) {
+    console.log("wqeqeqwe");
+    // const result = vscode.languages.getLanguages().then((value: string[]) => {
+    //     const showInfoNotificationAsModal = vscode.commands.registerCommand('notifications-sample.showInfoAsModal', () => {
+    //         vscode.window.showInformationMessage(value.toString(), { modal: true });
+    //     });
+    //     context.subscriptions.push(showInfoNotificationAsModal);
+    // });
+    
+}
